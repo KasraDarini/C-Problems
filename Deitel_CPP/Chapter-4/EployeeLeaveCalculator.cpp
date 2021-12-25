0f3117cdff9f8b9a738de17c0318becfaf31cfca
Kasra Darini <71825544+KasraDarini@users.noreply.github.com>
2021-12-25 13:22:44 +0330

    Update EployeeLeaveCalculator.cpp

--- Deitel_CPP/Chapter-4/EployeeLeaveCalculator.cpp
+++ Deitel_CPP/Chapter-4/EployeeLeaveCalculator.cpp
@@ -2,5 +2,16 @@
 using namespace std; 
 
 int main(){
-    
+    double hoursWorked{0};
+    cout << "Enter the hours worked: "; 
+    cin >> hoursWorked ; 
+    while(hoursWorked != -1.00){
+        double res; 
+        res= (hoursWorked/10) +2.00 ; 
+        cout << "hours you get: "<<res << endl ;
+        cout << endl ;
+        cout <<"Enter the hours worked: "; 
+        cin >> hoursWorked ; 
+    } 
+    return 0;
 }
